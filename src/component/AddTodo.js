c409d1296cf7a5cee42d3066e45f05d0f8f8a880
ddrewi <84168601+ddrewi@users.noreply.github.com>
2024-07-08 00:07:55 +0800

    Modified AddTodo on the frontend to communicate with the backend

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -3,6 +3,8 @@ import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
+import Axios from "axios";
+
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
   constructor() {
@@ -39,6 +41,24 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+      };
+      
+      Axios({
+        method: "POST",
+        url: "http://localhost:8080/add/item",
+        data: {jsonObject},
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => {
+        console.log(res.data.message);
+      });
+
       this.props.addTodo(this.state);
       this.setState({
         content: "",
